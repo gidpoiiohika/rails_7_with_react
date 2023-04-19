87ea90c5f2bd94acbd888ab18e2c370a369fa06d
Yaroslav Tomenchyk <yaroslav@webcap.com>
2023-04-19 11:37:10 +0200

    Replacing ReactDOM.render with createRoot

--- app/javascript/react/scr/components/QuestionDetail.jsx
+++ app/javascript/react/scr/components/QuestionDetail.jsx
@@ -3,11 +3,11 @@ import * as ReactDOM from 'react-dom'
 
 const QuestionDetail = (props) => {
   return(
-    <div className='card rounded-0 mt-3' key={question.id}>
+    <div className='card rounded-0 mt-3'>
       <div className='card-body'>
-        <h3 className='card-title'>{question.title}</h3>
+        <h3 className='card-title'>{props.question.title}</h3>
         <p className='lead'>
-          <span className='badge bg-primary'>{question.tag}</span>
+          <span className='badge bg-primary'>{props.question.tag}</span>
         </p>
       </div>
     </div>
--- app/javascript/react/scr/components/Welcome.jsx
+++ app/javascript/react/scr/components/Welcome.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import * as ReactDOM from 'react-dom'
+import * as ReactDOM from 'react-dom/client'
 import QuestionList from './QuestionList'
 
 const Welcome = () => {
@@ -10,8 +10,13 @@ const Welcome = () => {
   )
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(<Welcome />, document.getElementById('welcome'))
-})
+// document.addEventListener('DOMContentLoaded', () => {
+//   ReactDOM.render(<Welcome />, document.getElementById('welcome'))
+// })
+
+const root = ReactDOM.createRoot(document.getElementById('welcome'))
+root.render(
+  <Welcome />
+)
 
 export default Welcome
