ff459d7d61a63e28d1caaca04548a62c58e291bf
Yaroslav Tomenchyk <yaroslav@webcap.com>
2023-04-19 18:41:56 +0200

    Handling DOMException - Failed to execute removeChild

--- app/javascript/react/scr/components/EmptyQuestionMessage.jsx
+++ app/javascript/react/scr/components/EmptyQuestionMessage.jsx
@@ -3,9 +3,11 @@ import * as ReactDOM from 'react-dom'
 
 const EmptyQuestionMessagea = (props) => {
   return(
-    <div className="mt-5 alert alert-danger alert-dismissible fade show" role="alert">
-      <strong>OOps!</strong> No questions found with the tag: {props.tagname}. Please select another optionals list.
-      <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    <div>
+      <div className="mt-5 alert alert-danger alert-dismissible fade show" role="alert">
+        <strong>OOps!</strong> No questions found with the tag: {props.tagname}. Please select another optionals list.
+        <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>
     </div>
   )
 }
