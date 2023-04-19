651fae4ab306f1220ae9788f1c49c25adb2535e6
Yaroslav Tomenchyk <yaroslav@webcap.com>
2023-04-19 12:12:20 +0200

    Handling States in Class Components

--- app/javascript/react/scr/components/QuestionDetail.jsx
+++ app/javascript/react/scr/components/QuestionDetail.jsx
@@ -1,24 +1,41 @@
 import * as React from 'react'
-import { useState } from 'react'
+// import { useState } from 'react'
 import * as ReactDOM from 'react-dom'
 
-const QuestionDetail = (props) => {
-  const [likeCount, setLikeCount ] = useState(0)
+class QuestionDetail extends React.Component {
+  // const [likeCount, setLikeCount ] = useState(0)
 
-  return(
-    <div className='card rounded-0 mt-3'>
-      <div className='card-body'>
-        <h3 className='card-title'>{props.question.title}</h3>
-        <p className='lead'>
-          <span className='badge bg-primary'>{props.question.tag}</span>
-        </p>
-        <button className='btn btn-primary mt-1' onClick={() => setLikeCount(likeCount + 1)}>Like</button>
-        { likeCount > 0 ?
-          <span className='badge bg-primary'>{likeCount}</span> : ''
-        }
+  constructor(props) {
+    super(props)
+    this.state = { likeCount: 0}
+
+    this.updateLikeCount = this.updateLikeCount.bind(this)
+  }
+
+  updateLikeCount() {
+    this.setState(function(state){
+      return {
+        likeCount: state.likeCount + 1
+      }
+    })
+  }
+
+  render() {
+    return(
+      <div className='card rounded-0 mt-3'>
+        <div className='card-body'>
+          <h3 className='card-title'>{this.props.question.title}</h3>
+          <p className='lead'>
+            <span className='badge bg-primary'>{this.props.question.tag}</span>
+          </p>
+          <button className='btn btn-primary mt-1' onClick={this.updateLikeCount}>Like</button>
+          { this.state.likeCount > 0 ?
+            <span className='badge bg-primary'>{this.state.likeCount}</span> : ''
+          }
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default QuestionDetail
