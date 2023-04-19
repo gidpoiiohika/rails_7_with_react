1efa8353628a2e90f144c9a1e6315e7f195e198c
Yaroslav Tomenchyk <yaroslav@webcap.com>
2023-04-19 15:27:19 +0200

    Implement Like-Dislike Button for Questions

--- app/javascript/react/scr/components/QuestionDetail.jsx
+++ app/javascript/react/scr/components/QuestionDetail.jsx
@@ -7,15 +7,27 @@ class QuestionDetail extends React.Component {
 
   constructor(props) {
     super(props)
-    this.state = { likeCount: 0}
+    this.state = { 
+      likeCount: 0,
+      dislikeCount: 0
+    }
 
     this.updateLikeCount = this.updateLikeCount.bind(this)
+    this.updateDislikeCount = this.updateDislikeCount.bind(this)
   }
 
   updateLikeCount() {
     this.setState(function(state){
       return {
         likeCount: state.likeCount + 1
+      }
+    })
+  }
+
+  updateDislikeCount() {
+    this.setState(function(state){
+      return {
+        dislikeCount: state.dislikeCount + 1
       }
     })
   }
@@ -28,10 +40,22 @@ class QuestionDetail extends React.Component {
           <p className='lead'>
             <span className='badge bg-primary'>{this.props.question.tag}</span>
           </p>
-          <button className='btn btn-primary mt-1' onClick={this.updateLikeCount}>Like</button>
-          { this.state.likeCount > 0 ?
-            <span className='badge bg-primary'>{this.state.likeCount}</span> : ''
-          }
+          <button type="button" className="btn btn-primary position-relative" onClick={this.updateLikeCount} 
+            style={{marginRight: 1 + 'em'}}>
+            Like
+            { this.state.likeCount > 0 ?
+              <span className='position-absolute top-0 start-100 
+              translate-middle badge rounded-pill bg-danger'>{this.state.likeCount}</span> : ''
+            }
+          </button>
+
+          <button type="button" className="btn btn-primary position-relative" onClick={this.updateDislikeCount}>
+            Dislike
+            { this.state.dislikeCount > 0 ?
+              <span className='position-absolute top-0 start-100 
+              translate-middle badge rounded-pill bg-danger'>{this.state.dislikeCount}</span> : ''
+            }
+          </button>
         </div>
       </div>
     )
